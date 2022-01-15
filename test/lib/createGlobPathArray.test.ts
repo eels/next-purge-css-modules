b96486e0630b29c51eec0dda0fa2f6291b0c581d
Liam Howell <liam@liam.codes>
2022-01-15 22:59:44 +0000

    fix: update type in test description

--- test/lib/createGlobPathArray.test.ts
+++ test/lib/createGlobPathArray.test.ts
@@ -2,7 +2,7 @@ import createGlobPathArray from '@src/lib/createGlobPathArray';
 import path from 'path';
 
 describe('lib/createGlobPathArray', () => {
-  it('should return an arry of files matching the input of an array of directory globs', () => {
+  it('should return an array of files matching the input of an array of directory globs', () => {
     const glob = [path.join(__dirname, '../__mocks__/glob-files/**/*.txt')];
 
     expect(createGlobPathArray(glob)).toEqual([
